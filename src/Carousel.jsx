2c76dd2f234981e8514798d04cd9af834eacb690
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-15 01:09:52 +0530

    first commit

---
+++ src/Carousel.jsx
@@ -0,0 +1,61 @@
+import { shortList, list, longList } from "./data";
+import { useEffect, useState } from "react";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronRight, FiChevronLeft, FiChevronsRight } from "react-icons/fi";
+const Carousel = () => {
+  const [people, setPeople] = useState(list);
+  const [currPerson, setCurrPerson] = useState(0);
+  const prevSlide = () => {
+    setCurrPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
+  useEffect(() => {
+    let sliderid = setInterval(() => {
+      nextSlide();
+    }, 2000);
+    return () => {
+      clearInterval(sliderid);
+    };
+  }, [currPerson]);
+
+  return (
+    <section className="slider-container">
+      {people.map((person, personIndex) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIndex - currPerson)}%)`,
+              opacity: personIndex === currPerson ? 1 : 0,
+              visibility: personIndex === currPerson ? "visible" : "hidden",
+            }}
+            key={id}
+          >
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button type="button" className="prev" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <button type="button" className="next" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+};
+
+export default Carousel;
